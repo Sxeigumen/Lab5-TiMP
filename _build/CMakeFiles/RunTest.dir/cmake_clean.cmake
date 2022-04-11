b0e97ab55d41e9636e8fa24e53b0179477551f84
Sxeigumen <krasnov619@yandex.ru>
2022-04-11 21:21:46 +0300

    no comm

---
+++ _build/CMakeFiles/RunTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunTest.dir/tests/test.cpp.o"
+  "RunTest"
+  "RunTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RunTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/artem/workspace/Timp5/third-party/gtest/googletest
+# Build directory: /mnt/c/Users/artem/workspace/Timp5/_build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
